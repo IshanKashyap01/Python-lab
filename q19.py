afe09968efa7cac1079a3f717c902a8429a2acbc
IshanKashyap01 <83977077+IshanKashyap01@users.noreply.github.com>
2021-10-11 22:26:52 +0530

    Create q19.py
    Check if a name is in a list or not.

---
+++ q19.py
@@ -0,0 +1,6 @@
+nameList = ["Ishan", "Rahul", "Raj", "Janmesh"]
+name = input("Enter a name: ")
+if name in nameList:
+	print("The name is in the list")
+else:
+	print("The name is not in the list")
