abb877326704324fbfe45a273cd20868b627ef06
IshanKashyap01 <83977077+IshanKashyap01@users.noreply.github.com>
2021-10-11 21:24:06 +0530

    Create q10.py
    Dictionary with Hindi words and their English translations.

---
+++ q10.py
@@ -0,0 +1,5 @@
+dict = {'aaj': 'today', 'kal': 'tommorow', 'parso': 'day after tommorow'}
+for i in dict:
+	print(i)
+x = input("Enter any of the above hindi word to know their meaning: ")
+print(dict[x])
