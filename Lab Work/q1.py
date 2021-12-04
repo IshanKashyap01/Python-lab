757b9c2ef4a418c2961c84ebf42833d2c50eac71
IshanKashyap01 <83977077+IshanKashyap01@users.noreply.github.com>
2021-12-04 22:59:53 +0530

    Create q1.py

---
+++ Lab Work/q1.py
@@ -0,0 +1,8 @@
+a = 3.4536
+
+print ("The value of number till 2 decimal place(using %) is : ",end="")
+print ('%.2f'%a)
+print ("The value of number till 2 decimal place(using format()) is : ",end="")
+print ("{0:.2f}".format(a))
+print ("The value of number till 2 decimal place(using round()) is : ",end="")
+print (round(a,2))
